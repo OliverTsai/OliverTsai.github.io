9f8b20db1e250c053c0d9b0583f7668c76bbbac3
OliverTsai <qazpol886@gmail.com>
2022-10-12 23:42:48 +0800

    更新

--- script.js
+++ script.js
@@ -1,49 +1,17 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
 
-let currentActive = 1
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
-next.addEventListener('click', () => {
-    currentActive++
-
-    if(currentActive > circles.length) {
-        currentActive = circles.length
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement('div')
+      box.classList.add('box')
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      boxesContainer.appendChild(box)
     }
-
-    update()
-})
-
-prev.addEventListener('click', () => {
-    currentActive--
-
-    if(currentActive < 1) {
-        currentActive = 1
-    }
-
-    update()
-})
-
-function update() {
-    circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
-
-    if(currentActive === 1) {
-        prev.disabled = true
-    } else if(currentActive === circles.length) {
-        next.disabled = true
-    } else {
-        prev.disabled = false
-        next.disabled = false
-    }
+  }
 }
+
+createBoxes()
